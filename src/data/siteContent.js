5a7a096d4760df3f48ead175124130a5409d2e90
Ayush <ayushsahu0716@outlook.com>
2026-05-22 00:53:38 +0100

    Add new chess project

--- src/data/siteContent.js
+++ src/data/siteContent.js
@@ -1,5 +1,20 @@
 export const projects = [
     {
+        title: "3D Chess",
+        category: "Game Front-End",
+        year: "2026",
+        summary: "3D Chess that supports single player and 2 player.",
+        impact: [
+            "Designed a cleaner interaction model for posting and discovery.",
+            "Built a responsive interface with a strong mobile-first layout.",
+            "Focused on speed, hierarchy, and a premium visual finish.",
+        ],
+        stack: [ "React", "CSS", "THREE", "Drei", "React Three Fiber"],
+        preview: "/images/Chess.png",
+        liveUrl: "https://3d-chess-js.netlify.app",
+        sourceUrl: "https://github.com/Ayxsh-S/3D-Chess",
+    },
+    {
         title: "Social Media App",
         category: "Full-Stack Social Platform",
         year: "2026",
@@ -31,7 +46,7 @@ export const projects = [
     },
     {
         title: "Where's Waldo",
-        category: "Game",
+        category: "Game Full-Stack",
         year: "2026",
         summary: "An interactive photo-tagging game inspired by the classic Where’s Waldo challenge, built around speed, accuracy, and replayability.",
         impact: [
@@ -44,21 +59,21 @@ export const projects = [
         liveUrl: "https://wheres-waldo-67.netlify.app",
         sourceUrl: "https://github.com/Ayxsh-S/waldo-frontend/",
     },
-    {
-        title: "File Uploader",
-        category: "Backend File Management",
-        year: "2026",
-        summary: "A file organization tool for creating folders, uploading files, and managing storage through a simple, structured workflow.",
-        impact: [
-            "Designed a cleaner interaction model for posting and discovery.",
-            "Built a responsive interface with a strong mobile-first layout.",
-            "Focused on speed, hierarchy, and a premium visual finish.",
-        ],
-        stack: [ "Node", "EJS", "PostgreSQL", "Prisma", "Passport.js", "Multer", "Express"],
-        preview: "/images/Drive.png",
-        liveUrl: "https://drive-f9iw.onrender.com",
-        sourceUrl: "https://github.com/Ayxsh-S/drive",
-    },
+    // {
+    //     title: "File Uploader",
+    //     category: "Backend File Management",
+    //     year: "2026",
+    //     summary: "A file organization tool for creating folders, uploading files, and managing storage through a simple, structured workflow.",
+    //     impact: [
+    //         "Designed a cleaner interaction model for posting and discovery.",
+    //         "Built a responsive interface with a strong mobile-first layout.",
+    //         "Focused on speed, hierarchy, and a premium visual finish.",
+    //     ],
+    //     stack: [ "Node", "EJS", "PostgreSQL", "Prisma", "Passport.js", "Multer", "Express"],
+    //     preview: "/images/Drive.png",
+    //     liveUrl: "https://drive-f9iw.onrender.com",
+    //     sourceUrl: "https://github.com/Ayxsh-S/drive",
+    // },
     {
         title: "Messaging App",
         category: "Full-stack Social Platform",
